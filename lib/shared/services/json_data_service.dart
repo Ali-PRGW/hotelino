99d79ec0ca80f5f3b3e400da370bc148e470f01f
Ali Ramezani <ali1381ramezaniali@gmail.com>
2026-06-14 23:09:33 +0330

    add json data service

---
+++ lib/shared/services/json_data_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:hotelino/core/constants/constants.dart';
+import 'package:hotelino/features/home/data/models/hotel.dart';
+
+class JsonDataService {
+
+  Future<List<Hotel>> loadHotels() async{
+    final String response = await rootBundle.loadString(AppConstants.hotelData);
+    final Map<String , dynamic> jsonData = json.decode(response);
+
+    final List<dynamic> hotelList = jsonData["hotels"];
+
+    return hotelList.map(
+      (hotel) => Hotel.fromJson(hotel as Map<String , dynamic>) ,
+    )
+    .toList();
+
+  }
+}
