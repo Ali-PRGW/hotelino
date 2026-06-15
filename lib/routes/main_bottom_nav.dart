c52df98a7d59e961c4bba52d8ca3abe03fb04222
Ali Ramezani <ali1381ramezaniali@gmail.com>
2026-06-15 16:37:47 +0330

    edit homepage directory

--- lib/routes/main_bottom_nav.dart
+++ lib/routes/main_bottom_nav.dart
@@ -3,7 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hotelino/core/constants/constants.dart';
 import 'package:hotelino/features/boking/bookingPage.dart';
 import 'package:hotelino/features/favorite/favoritePage.dart';
-import 'package:hotelino/features/home/homePage.dart';
+import 'package:hotelino/features/home/presentation/homePage.dart';
 import 'package:hotelino/features/profile/profilePage.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
