a714df77dd678346bebbe2fad837c30ea6d3def8
Ali Ramezani <ali1381ramezaniali@gmail.com>
2026-06-16 13:42:05 +0330

    finish home app bar

--- lib/features/home/presentation/widgets/home_appbar.dart
+++ lib/features/home/presentation/widgets/home_appbar.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/core/theme/theme_provider.dart';
+import 'package:hotelino/features/home/logic/providers/profile_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppbar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return AppBar(
       elevation: 0,
       automaticallyImplyLeading: false,
@@ -20,23 +25,59 @@ class HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
                     icon: Icon(Icons.notifications_none),
                     color: Colors.grey,
                   ),
+                  Consumer<ProfileProvider>(
+                    builder: (context, profileProvider, child) {
+                      return profileProvider.profileData?.notifications !=
+                                  null &&
+                              profileProvider.profileData!.notifications > 0
+                          ? Positioned(
+                              right: 12,
+                              top: 14,
+                              child: CircleAvatar(
+                                radius: 4,
+                                backgroundColor: Theme.of(
+                                  context,
+                                ).colorScheme.primary,
+                              ),
+                            )
+                          : SizedBox();
+                    },
+                  ),
                 ],
+              ),
+              IconButton(
+                onPressed: () {
+                  themeProvider.toggleTheme();
+                },
+                icon: Icon(
+                  themeProvider.brightness == Brightness.light
+                      ? Icons.dark_mode
+                      : Icons.light_mode,
+                  color: Colors.grey,
+                ),
               ),
             ],
           ),
 
-          Row(
-            children: [
-              Text(
-                'علی رمضانی',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              SizedBox(width: 8),
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: AssetImage('assets/images/profile.jpg'),
-              ),
-            ],
+          Consumer<ProfileProvider>(
+            builder: (context, profileProvider, child) {
+              return Row(
+                children: [
+                  Text(
+                    profileProvider.profileData?.name ?? 'کاربر',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  SizedBox(width: 8),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage(
+                      profileProvider.profileData?.avatarUrl ??
+                          'https://www.w3schools.com/howto/img_avatar.png',
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ],
       ),
