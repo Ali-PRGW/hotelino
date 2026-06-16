8d3b64405b747bb4919288b473af6e62f1c95d1d
Ali Ramezani <ali1381ramezaniali@gmail.com>
2026-06-16 22:12:56 +0330

    add hotel card to hotel list section

--- lib/features/home/presentation/widgets/hotel_list_section.dart
+++ lib/features/home/presentation/widgets/hotel_list_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hotelino/features/home/data/models/hotel.dart';
 import 'package:hotelino/features/home/logic/providers/favorite_item_provider.dart';
+import 'package:hotelino/features/home/presentation/widgets/hotel_card.dart';
 import 'package:provider/provider.dart';
 
 class HotelListSection extends StatelessWidget {
@@ -34,20 +35,25 @@ class HotelListSection extends StatelessWidget {
                   ),
                 ),
               ),
-              Text(title , style: Theme.of(context).textTheme.displayMedium,)
+              Text(title, style: Theme.of(context).textTheme.displayMedium),
             ],
           ),
         ),
-        SizedBox(height: 360,child: ListView.builder(
-          itemCount: hotels.length,
-          itemBuilder: (context, index) {
-            return Consumer<FavoriteItemProvider>(
-
-             );
-          },
+        SizedBox(
+          height: 360,
+          child: ListView.builder(
+            padding: EdgeInsets.only(right: 16),
+            reverse: true,
+            clipBehavior: Clip.none,
+            scrollDirection: Axis.horizontal,
+            itemCount: hotels.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsetsGeometry.only(left: 16),
+                child: HotelCard(hotel: hotels[index]));
+            },
+          ),
         ),
-      )
-
       ],
     );
   }
