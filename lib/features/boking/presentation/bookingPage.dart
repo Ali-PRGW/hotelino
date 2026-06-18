5d0817752f4948bf5ef71bc538721817fe967c3c
Ali Ramezani <ali1381ramezaniali@gmail.com>
2026-06-18 12:20:06 +0330

    add DatePickerField to booking page

--- lib/features/boking/presentation/bookingPage.dart
+++ lib/features/boking/presentation/bookingPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hotelino/features/boking/logic/booking_provider.dart';
 import 'package:hotelino/features/boking/presentation/widgets/booking_form_field.dart';
+import 'package:hotelino/features/boking/presentation/widgets/date_picker_field.dart';
 import 'package:provider/provider.dart';
 
 class BookingPage extends StatefulWidget {
@@ -82,24 +83,25 @@ class _BookingPageState extends State<BookingPage> {
 
                     SizedBox(height: 8),
 
-                      BookingFormField(
-                      title: 'نام و نام خانوادگی ',
-                      hint: 'نام و نام خانوادگی خود را وارد کنید ...',
-                      initialValue: bookingProvider.booking.fullName,
-                      keyboardType: TextInputType.text,
+                    DatePickerField(
+                      title: 'تاریخ اقامت',
+                      hint: 'بازه زمانی اقامت را مشخص کنید',
+                      initialValue: bookingProvider.booking.checkInOutRangeDate,
                       validator: (value) {
-                        if( value==null || value.isEmpty){
-                          return 'لطفا نام خود را کامل بنویسید';
+                        if (value == null) {
+                          return 'لطفا بازه زمانی را انتخاب کنید:';
                         }
 
                         return null;
                       },
                       onSaved: (newValue) {
-                        if(newValue != null){
-                          bookingProvider.setName(newValue);
+                        if (newValue != null) {
+                          bookingProvider.setRangeDate(newValue);
                         }
                       },
                     ),
+
+                    SizedBox(height: 8),
                   ],
                 ),
               );
